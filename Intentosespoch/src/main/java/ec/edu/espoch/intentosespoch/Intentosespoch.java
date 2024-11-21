077819c4df2983ac249f2b44e8b546fb63f49a44
Dilankl30 <lucerodk7@gmail.com>
2024-11-21 11:36:35 -0500

    actualizacion

--- Intentosespoch/src/main/java/ec/edu/espoch/intentosespoch/Intentosespoch.java
+++ Intentosespoch/src/main/java/ec/edu/espoch/intentosespoch/Intentosespoch.java
@@ -13,5 +13,6 @@ public class Intentosespoch {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("intentos y mas intentos");
+        System.out.println("holas iahdiusahihasdpioashd");
     }
 }
